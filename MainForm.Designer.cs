66c8bfc339b59cdd9b232e23acfea0c7f8ff0f89
pypy <git@pypy.moe>
2020-01-26 15:47:22 +0900

    initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace VRC_YTDL_Updater
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LocalVersion = new System.Windows.Forms.Label();
+            this.RemoteVersion = new System.Windows.Forms.Label();
+            this.DownloadProgressBar = new System.Windows.Forms.ProgressBar();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.DownloadStatus = new System.Windows.Forms.Label();
+            this.pypy = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LocalVersion
+            // 
+            this.LocalVersion.AutoSize = true;
+            this.LocalVersion.Location = new System.Drawing.Point(120, 12);
+            this.LocalVersion.Name = "LocalVersion";
+            this.LocalVersion.Size = new System.Drawing.Size(11, 12);
+            this.LocalVersion.TabIndex = 0;
+            this.LocalVersion.Text = "-";
+            // 
+            // RemoteVersion
+            // 
+            this.RemoteVersion.AutoSize = true;
+            this.RemoteVersion.Location = new System.Drawing.Point(120, 36);
+            this.RemoteVersion.Name = "RemoteVersion";
+            this.RemoteVersion.Size = new System.Drawing.Size(11, 12);
+            this.RemoteVersion.TabIndex = 1;
+            this.RemoteVersion.Text = "-";
+            // 
+            // DownloadProgressBar
+            // 
+            this.DownloadProgressBar.Location = new System.Drawing.Point(14, 62);
+            this.DownloadProgressBar.Name = "DownloadProgressBar";
+            this.DownloadProgressBar.Size = new System.Drawing.Size(358, 23);
+            this.DownloadProgressBar.TabIndex = 3;
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Enabled = false;
+            this.UpdateButton.Location = new System.Drawing.Point(297, 126);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(75, 23);
+            this.UpdateButton.TabIndex = 4;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // DownloadStatus
+            // 
+            this.DownloadStatus.AutoSize = true;
+            this.DownloadStatus.Location = new System.Drawing.Point(12, 91);
+            this.DownloadStatus.Name = "DownloadStatus";
+            this.DownloadStatus.Size = new System.Drawing.Size(41, 12);
+            this.DownloadStatus.TabIndex = 5;
+            this.DownloadStatus.Text = "Ready";
+            // 
+            // pypy
+            // 
+            this.pypy.AutoSize = true;
+            this.pypy.Location = new System.Drawing.Point(12, 132);
+            this.pypy.Name = "pypy";
+            this.pypy.Size = new System.Drawing.Size(265, 12);
+            this.pypy.TabIndex = 6;
+            this.pypy.TabStop = true;
+            this.pypy.Text = "https://github.com/pypy-vrc/vrc_ytdl_updater";
+            this.pypy.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.pypy_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Latest Version";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Installed Version";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pypy);
+            this.Controls.Add(this.DownloadStatus);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.DownloadProgressBar);
+            this.Controls.Add(this.RemoteVersion);
+            this.Controls.Add(this.LocalVersion);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VRChat youtube-dl updater (v0.0.1)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LocalVersion;
+        private System.Windows.Forms.Label RemoteVersion;
+        private System.Windows.Forms.ProgressBar DownloadProgressBar;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Label DownloadStatus;
+        private System.Windows.Forms.LinkLabel pypy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+#nullable disable
+
+namespace VRC_YTDL_Updater
+{
+    public partial class MainForm : Form
+    {
+        private string LocalFilePath = string.Empty;
+        private string RemoteFileURL = string.Empty;
+        private bool Updating = false;
+
+        public MainForm()
+        {
+            // specify to use TLS 1.2 as default connection
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetLocalVersion();
+            GetRemoteVersion();
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            UpdateFile();
+        }
+
+        private void GetLocalVersion()
+        {
+            LocalFilePath = @"C:\Program Files (x86)\Steam\steamapps\common\VRChat\VRChat_Data\StreamingAssets\youtube-dl.exe";
+
+            try
+            {
+                using (var key = Registry.ClassesRoot.OpenSubKey(@"VRChat\shell\open\command"))
+                {
+                    // "C:\Program Files (x86)\Steam\steamapps\common\VRChat\launch.bat" "C:\Program Files (x86)\Steam\steamapps\common\VRChat" "%1"
+                    var match = Regex.Match(key.GetValue(string.Empty) as string, "^\"(.+\\\\VRChat)\\\\launch.bat\"");
+                    if (match.Success)
+                    {
+                        var path = match.Groups[1].Value;
+                        LocalFilePath = path + @"\VRChat_Data\StreamingAssets\youtube-dl.exe";
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                var info = FileVersionInfo.GetVersionInfo(LocalFilePath);
+                LocalVersion.Text = info.FileVersion;
+            }
+            catch
+            {
+                LocalVersion.Text = "Error";
+            }
+        }
+
+        private async void GetRemoteVersion()
+        {
+            ReleaseDTO release = null;
+
+            BeginInvoke((MethodInvoker)delegate
+            {
+                RemoteVersion.Text = "Checking";
+            });
+
+            try
+            {
+                using (var http = new HttpClient())
+                {
+                    var headers = http.DefaultRequestHeaders;
+                    headers.ExpectContinue = true;
+                    headers.Add("User-Agent", "VRC_YTDL_Updater");
+                    var response = await http.GetAsync(
+                        "https://api.github.com/repos/ytdl-org/youtube-dl/releases/latest",
+                        new CancellationTokenSource(3000).Token
+                        );
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        using (var raw = await response.Content.ReadAsStreamAsync())
+                        {
+                            using (var stream = new StreamReader(raw))
+                            {
+                                using (var reader = new JsonTextReader(stream))
+                                {
+                                    var serializer = new JsonSerializer();
+                                    release = serializer.Deserialize<ReleaseDTO>(reader);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+            BeginInvoke((MethodInvoker)delegate
+            {
+                if (release != null)
+                {
+                    foreach (var asset in release.assets)
+                    {
+                        if ("youtube-dl.exe".Equals(asset.name, StringComparison.OrdinalIgnoreCase))
+                        {
+                            RemoteFileURL = asset.browser_download_url;
+                            var size = Math.Round(asset.size / 1048576f, 2);
+                            RemoteVersion.Text = $"{release.tag_name} ({size} MiB)";
+                            UpdateButton.Enabled = true;
+                            return;
+                        }
+                    }
+                }
+                RemoteVersion.Text = "Error";
+            });
+        }
+
+        private async void UpdateFile()
+        {
+            if (Updating)
+            {
+                return;
+            }
+
+            Updating = true;
+
+            BeginInvoke((MethodInvoker)delegate
+            {
+                UpdateButton.Enabled = false;
+                DownloadStatus.Text = "Downloading";
+            });
+
+            var wasError = false;
+
+            try
+            {
+                var client = new WebClient();
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloadProgressChanged);
+                await client.DownloadFileTaskAsync(new Uri(RemoteFileURL), $"{LocalFilePath}.new");
+                File.Delete(LocalFilePath);
+                File.Move($"{LocalFilePath}.new", LocalFilePath);
+            }
+            catch
+            {
+                wasError = true;
+            }
+
+            BeginInvoke((MethodInvoker)delegate
+            {
+                DownloadStatus.Text = wasError ? "Error" : "Completed";
+                UpdateButton.Enabled = true;
+                GetLocalVersion();
+                GetRemoteVersion();
+            });
+
+            Updating = false;
+        }
+
+        private void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            BeginInvoke((MethodInvoker)delegate
+            {
+                var pct = 0;
+                if (e.TotalBytesToReceive > 0)
+                {
+                    pct = (int)((double)e.BytesReceived / e.TotalBytesToReceive * 100);
+                }
+                var size = Math.Round(e.BytesReceived / 1048576f, 2);
+                DownloadStatus.Text = $"Downloading {pct}% ({size}MiB)";
+                DownloadProgressBar.Value = pct;
+            });
+        }
+
+        private void pypy_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(((LinkLabel)sender).Text).Close();
+        }
+    }
+
+    public class ReleaseDTO
+    {
+        public string tag_name;
+        public IList<ReleaseAssetDTO> assets;
+    }
+
+    public class ReleaseAssetDTO
+    {
+        public string name;
+        public ulong size;
+        public string browser_download_url;
+    }
+}
